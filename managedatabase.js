afe0b5c6036ac939941d156630c293cef3f799b3
Tim Brady <tfb414@gmail.com>
2017-08-18 08:37:15 -0400

    removes some unused code

--- managedatabase.js
+++ managedatabase.js
@@ -30,33 +30,6 @@ function addSurveyAndHostToDatabase(surveyID, hostID){
   `).then(result=>{return result.survey_id}).catch(console.log);
 }
 
-
- 
-function addQuestionsToDatabase(receivedSurvey){
-  var counter = 1;
-  var questionsToAdd = [];
-  for (submitted in receivedSurvey){
-    if(questionEqualsQuestion(receivedSurvey[submitted], receivedSurvey['question ' + counter])){
-      questionsToAdd.push(receivedSurvey['question ' + counter]);
-      counter++
-    }
-  }
-  addQuestionArrayToDB(questionsToAdd);
-}
-  
-function addAnswersToDatabase(receivedSurvey){
-  var counter = 1;
-  var answersToAdd = [];
-  for(submitted in receivedSurvey){
-    if(questionEqualsQuestion(receivedSurvey[submitted], receivedSurvey['answer' + counter])){
-      answersToAdd.push(receivedSurvey['answer' + counter]);
-      counter++;
-    }
-  }
-  addAnswerstoDB(answersToAdd);
-}
-
-
 function addClientName(clientName){
   return db.one(`
       INSERT INTO clients(client_name) 
@@ -65,29 +38,12 @@ function addClientName(clientName){
   `).catch(console.log)
 }
 
-function questionEqualsQuestion(thing1, thing2){
-  return thing1 === thing2
-}
-
-function addQuestionArrayToDB(array){
-  array.forEach((element)=>{
-    db.one(`
-    insert into questions(question)
-      values ('${element}')
-      returning question_id
-    `)
-  });
-}
-
-
 function addQuestionsAnswersSurveyToDB(data, surveyID){
   if (Object.keys(data).length < 2){
     return;
   }
-  // console.log(Object.values(surveyID) + 'values');
-  // console.log(Object.keys(surveyID) + 'keys');
   Object.keys(data.question).forEach((qAndA)=>{
-    // console.log(data.question[qAndA]['text'] + "HERE");
+    
     questionID = db.one(`
       insert into questions(question)
         values ('${data.question[qAndA]['text']}')
@@ -133,7 +89,6 @@ function sendFormDataToDB(dataFromForm, uniqueid){
       .then(survey_id =>{addSurveyAndHostToDatabase(survey_id, uniqueid)
         .then(survey_id =>{addQuestionsAnswersSurveyToDB(dataFromForm, survey_id)})
       })
-      
       .catch(console.log);   
 }
 
@@ -143,31 +98,6 @@ module.exports = {
     addHostToDatabase: addHostToDatabase, 
     addClientName: addClientName,
     addSurveyToDatabase: addSurveyToDatabase,
-    addQuestionsToDatabase:addQuestionsToDatabase,
-    addAnswersToDatabase:addAnswersToDatabase
+    
   };
 
-
-  function addClientName(object){
-    return client_id = db.one(`
-        INSERT INTO clients(client_name) 
-        VALUES('${object.client_name}') 
-        RETURNING client_id;
-    `).then(client_id => {
-      console.log([client_id][0].client_id)
-      console.log(object)
-      return host_id = db.query(`
-          SELECT host_id 
-          FROM hosts 
-          WHERE host_unique_id = '${object.host_id}';
-  `).then(host_id => {
-        console.log(host_id[0].host_id)     
-        console.log([client_id][0].client_id)
-        return db.one(`
-            INSERT INTO client_host(client_id, host_id) 
-            VALUES(${[client_id][0].client_id}, ${host_id[0].host_id}) 
-            RETURNING client_id, host_id
-    `)
-    })
-    })
-  }
