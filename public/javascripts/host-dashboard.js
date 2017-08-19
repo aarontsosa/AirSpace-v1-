26d55eb132a336496e53105e3d5a5d446444f929
Aaron <aarontsosa@outlook.com>
2017-08-19 13:47:25 -0400

    added the ability to send diffrent surveys to everyone

--- public/javascripts/host-dashboard.js
+++ public/javascripts/host-dashboard.js
@@ -4,18 +4,17 @@ function sendToWebSocket(message){
     socket.send(JSON.stringify(message));
 }
 
-$("[data-target='activate-survey']").click(()=>{
+$("[data-target='activate-survey']").on('click', (event) =>{
     var urlPathParts = window.location.pathname.split("/");
     var uniqueID = urlPathParts[urlPathParts.length - 2];
-    var surveyID = $("[data-target='activate-survey']").data("survey-id");
-    console.log(urlPathParts);
-    console.log(surveyID);
+    var surveyID = event.target.attributes[2].value
     var sendToServer = {
         [uniqueID]: {
             'ID': uniqueID,
             'survey_id': surveyID 
         }
     }
+    console.log(sendToServer)
     sendToWebSocket(sendToServer);
 
 
