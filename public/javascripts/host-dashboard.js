dd5e57cdef2541337b4b76b89c8b818331d85a39
Tim Brady <tfb414@gmail.com>
2017-08-22 21:15:49 -0400

    cleans up code

--- public/javascripts/host-dashboard.js
+++ public/javascripts/host-dashboard.js
@@ -4,18 +4,6 @@ function sendToWebSocket(message){
     socket.send(JSON.stringify(message));
 }
 
-// $(window).load( () => {
-//     if ($('body').height() - $('.video-holder').height() > 0) {
-//         $('.video-holder').css( "height", $('body').height() + 300)
-//     }
-// }
-
-// $(document).ready( () => {
-//     $('.non-button').click( () => {
-        
-//     });
-// });
-
 
 $("[data-target='activate-survey']").on('click', (event) =>{
     var urlPathParts = window.location.pathname.split("/");
@@ -27,25 +15,8 @@ $("[data-target='activate-survey']").on('click', (event) =>{
             'survey_id': surveyID 
         }
     }
-    var hello = `
-    im DEVELOPINGGGG
-    `;
-    if ($('#active-users').css('display') == 'none') {
-        $(elementId).css('display', 'block')
-    $('#active-users').html(hello);
-    $('#active-users').css({
-        border: "1px solid solid white",
-        padding: "1rem",
-        "font-size": "1rem"
-    });
     sendToWebSocket(sendToServer);
 
-// $("#createsurvey").on('click', (event) => {
-//     location.replace(http://localhost:3001/host/{{uniqueid}}/{{id}}/surveynew)
-// })
-    // message = {
-    //     'derp': window.location
-    // }
-    // console.log(message);
-})
+    
+});
 
