8aadf07e7456ce0af89e6642fa7e7de0d3a67bb8
Tim Brady <tfb414@gmail.com>
2017-08-23 12:41:58 -0400

    ec2

--- routes/client.js
+++ routes/client.js
@@ -60,6 +60,7 @@ router.get('/:hostid/:name/:survey', function(req, res, next){
 })
 
 router.post('/:hostid/:name/:survey', function (req, res, next){
+    var socket = new ws('ws://ec2-18-220-45-149.us-east-2.compute.amazonaws.com:3002');
     manageDB.addResults(req.body['result'], req.params.name, req.params.survey)
     .then((result) => {
     var resultRequest = {
@@ -70,7 +71,10 @@ router.post('/:hostid/:name/:survey', function (req, res, next){
         }
     }
     console.log(resultRequest)
-    socket.send(JSON.stringify(resultRequest));
+    socket.on('open', function(){
+        socket.send(JSON.stringify(resultRequest));
+    })
+    
     })
 
     
