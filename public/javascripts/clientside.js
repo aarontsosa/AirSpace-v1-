04f8d8eae9937e512fdd41647979985cc8dc386c
Tim Brady <tfb414@gmail.com>
2017-08-31 16:24:53 -0400

    fixes ec3

--- public/javascripts/clientside.js
+++ public/javascripts/clientside.js
@@ -1,4 +1,5 @@
-let socket = new WebSocket('ws://'+ window.location.hostname + ':3002');
+// let socket = new WebSocket('ws://'+ window.location.hostname + ':3002');
+let socket = new WebSocket('ws://ec2-18-220-45-149.us-east-2.compute.amazonaws.com:3002');
 
 console.log("hello")
 function format(data){
